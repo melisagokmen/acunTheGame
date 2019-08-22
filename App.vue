e17c6a876f420716284f5036c6dfa96627a5ad98
altux5 <53811794+altux5@users.noreply.github.com>
2019-08-22 10:37:43 +0300

    hamdi bey has joined the game

--- App.vue
+++ App.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <h1>Acun The Game</h1>
-    <br />
+    <br/>
     <p class="box"></p>
+    <p class="remaining"></p>
+    <p class="offer"></p>
+    <button @click="openBox(boxes)">open a box</button>
   </div>
 </template>
 <script>
@@ -11,7 +14,8 @@ import axios from "axios";
 export default {
   data() {
     return {
-      boxes: []
+      boxes: [],
+      counter: 1,
     };
   },
   methods: {
@@ -23,13 +27,52 @@ export default {
     },
 
     drawBoxes () {
-      document.querySelector('.box').innerHTML = this.buildList(this.boxes)
+      document.querySelector('.remaining').innerHTML = '<br>' + 'remaining boxes: 24';
+  },
+
+    openBox(boxes) {
+      var random = Math.floor(Math.random() * boxes.length);
+      if(!boxes[random]){
+        document.querySelector('.remaining').innerHTML = 'out of box!';
+        return;
+      }
+      document.querySelector('.remaining').innerHTML = "opened box's worth: " +  (this.boxes[random].worth) + "₺" + "<br>" + 'remaining boxes: ' + (24-this.counter);
+      if(this.counter<24){
+      this.counter++;
+      }
+      if(this.counter==6 || this.counter==11 || this.counter==16 || this.counter==19 || this.counter==23){
+        this.hamdiBey(boxes);
+      }
+      else{
+        document.querySelector('.offer').innerHTML = " ";
+      }
+      boxes.splice(random,1);
+    },
+
+  hamdiBey(boxes) {
+      var potential = 0;
+      var maxWorth = 0;
+      var offer = 0;
+      var i = 0;
+      for( i=0 ;i<boxes.length;i++){
+        potential += boxes[i].worth;
+        if(boxes[i].worth>maxWorth){
+          maxWorth = boxes[i].worth;
+        }
+      }
+      offer = ((maxWorth/(i*(i-1)))+(potential/200+i));
+      offer = offer/100
+      offer = parseInt(offer)
+      alert("Hamdi Bey Teklif yapıyor!")
+      document.querySelector('.offer').innerHTML = "Hamdi Bey's offer: " + parseInt(offer*100) + "₺"
+      potential = 0;
   }
+
   },
   mounted() {
     axios.get("http://localhost:3400/").then(response => {
       this.boxes = response.data;
-      this.drawBoxes()
+      this.drawBoxes();
     });
   }
 }
--- api/acun_api.js
+++ api/acun_api.js
@@ -6,100 +6,100 @@ app.use(express.bodyParser());
 
 var boxes = [
     {
+        id: '0',
+        worth: 1,
+    },
+    {
         id: '1',
-        worth: '1₺',
+        worth: 5,
     },
     {
         id: '2',
-        worth: '5₺',
+        worth: 10,
     },
     {
-        id: '3',
-        worth: '10₺',
+        id:'3',
+        worth: 20,
     },
     {
         id:'4',
-        worth: '20₺',
+        worth: 50,
     },
     {
         id:'5',
-        worth: '50₺',
+        worth: 100,
     },
     {
         id:'6',
-        worth: '100₺',
+        worth: 200,
     },
     {
         id:'7',
-        worth: '200₺',
+        worth: 500,
     },
     {
         id:'8',
-        worth: '500₺',
+        worth: 1000,
     },
     {
         id:'9',
-        worth: '1000₺',
+        worth: 2000,
     },
     {
         id:'10',
-        worth: '2000₺',
+        worth: 5000,
     },
     {
         id:'11',
-        worth: '5000₺',
+        worth: 10000,
     },
     {
         id:'12',
-        worth: '10000₺',
+        worth: 20000,
     },
     {
         id:'13',
-        worth: '20000₺',
+        worth: 50000,
     },
     {
         id:'14',
-        worth: '50000₺',
+        worth: 100000,
     },
     {
         id:'15',
-        worth: '100000₺',
+        worth: 200000,
     },
     {
         id:'16',
-        worth: '200000₺',
+        worth: 500000,
     },
     {
         id:'17',
-        worth: '500000₺',
+        worth: 500000,
     },
     {
         id:'18',
-        worth: '500000₺',
+        worth: 500000,
     },
     {
         id:'19',
-        worth: '500000₺',
+        worth: 500000,
     },
     {
         id:'20',
-        worth: '500000₺',
+        worth: 1000000,
     },
     {
         id:'21',
-        worth: '1000000₺',
+        worth: 1000000,
     },
     {
         id:'22',
-        worth: '1000000₺',
+        worth: 1000000,
     },
     {
         id:'23',
-        worth: '1000000₺',
-    },
-    {
-        id:'24',
-        worth: '5000000₺',
+        worth: 5000000,
     }
 
 ]
@@ -107,6 +107,5 @@ var boxes = [
 app.get('/', function (req, res) {
     res.json(boxes);
   });
-
 
   app.listen(process.env.PORT || 3400);
